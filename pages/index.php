ca0e60db09529f2e0d483681b2aa59ef38316585
Paige Israel <paige.israel@yahoo.com>
2016-03-19 03:09:56 -0700

    Starting content on index.php
    1. Deleted (and stored) old content from index.php page.
    2. Started new content for index page.
    3. Resized picture for index page.

---
+++ pages/index.php
@@ -0,0 +1,14 @@
+<?php
+    $page_title = 'Paige Israel';
+    include ('../shared/header.html');
+?>
+// [Start content]      
+            
+		    <p>
+		    <a href="contact.html" alt="Contact link">Contact me </a>and we can talk to guarantee that your Web Site is all you desire!
+		    </p> 
+            </div>
+        
+    </body>
+        
+</html>
