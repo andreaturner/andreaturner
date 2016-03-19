0991a241c276dd549cdc2c80b44e0a866732c50e
Paige Israel <paige.israel@yahoo.com>
2016-03-18 17:37:18 -0700

    Worked on accordion on portfolio page
    1. Cropped images for Quilting Bee and CMF.
    2. Set accordion up on Portfolio page.
    3. Added css appropriately.

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -1,5 +1,37 @@
 <?php
-    $page_title = 'Contact';
+    $page_title = 'Portolio';
     include ('../shared/header.html');
 	 
 ?>
+<!-- [Stuff I've done....] -->
+<div class="header-text-blue">
+                Portfolio
+</div>
+            
+    <div id="portfolio">
+        
+     <h3 style="font-weight: bold;">Quilting Bee</h3>
+     
+        <a href="http://ned.highline.edu/~paigekeys/Portfolio/QuiltingBee/Quilt/index.html">
+            <img src="quiltingBee-173x224.png width="173' height="224" alt="Quilting Bee Image"></a>
+        
+    <h3 style="font-weight: bold;">Community Micro Finance</h3>
+    
+        <a href="http://ned.highline.edu/~paigekeys/Portfolio/CMF/index.html">
+            <img src="CMF-183x153.png width="183" height="153" alt="Community Micro Finance Logo"></a>
+        
+    </div>
+    
+        
+
+<script type="text/javascript">
+			//shorthand version - everybody uses this one in real life
+			$(function(){
+				$( "#portfolio" ).accordion({
+					heightStyle: 'content',
+					active: false,
+					collapsible: true
+				});
+			});			
+			
+		</script>   
