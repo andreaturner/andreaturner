384f47551265c04ae76d1e7a6461d3c15257929c
Paige Israel <paige.israel@yahoo.com>
2016-03-18 18:54:54 -0700

    Worked on portfolio page
    1. Resized Quilting Bee image.
    2. Resized CMF image.
    3. Resized Iftmus image.
    4. Resized KFB image.
    5. Adjusted styles for accordion in styles.

--- pages/portfolio.php
+++ pages/portfolio.php
@@ -1,30 +1,50 @@
 <?php
-    $page_title = 'Portolio';
+    $page_title = 'Portfolio';
     include ('../shared/header.html');
 	 
 ?>
 <!-- [Stuff I've done....] -->
 <div class="header-text-blue">
-                Portfolio
+    Portfolio
 </div>
             
     <div id="portfolio">
         
-     <h3 style="font-weight: bold;">Quilting Bee</h3>
-     
-        <a href="http://ned.highline.edu/~paigekeys/Portfolio/QuiltingBee/Quilt/index.html">
-            <img src="quiltingBee-173x224.png width="173' height="224" alt="Quilting Bee Image"></a>
+     <h3 style="font-weight: bold;">The Quilting Bee</h3>
+        <div>
+           <a href="http://ned.highline.edu/~paigekeys/Portfolio/QuiltingBee/Quilt/index.html">
+           <img src="../images/quiltingBee-100x130.png" width="100" height="130" alt="Quilting Bee Image"></a> 
+        </div>
         
     <h3 style="font-weight: bold;">Community Micro Finance</h3>
-    
-        <a href="http://ned.highline.edu/~paigekeys/Portfolio/CMF/index.html">
-            <img src="CMF-183x153.png width="183" height="153" alt="Community Micro Finance Logo"></a>
+        <div>
+            <a href="http://ned.highline.edu/~paigekeys/Portfolio/CMF/index.html">
+            <img src="../images/CMF-101x84.png" width="101" height="84" alt="Community Micro Finance Logo"></a>
+        </div>
         
+    <h3 style="font-weight: bold;">Wordman Enterprise</h3>
+        <div>
+            <a href="http://paigeisrael.com/sites/wordman/pages/books-series.php">
+            <img src="../images/iftmus-100x147.jpg" width="100" height="147" alt="Iftmus Image"></a>
+        </div>
+    <h3 style="font-weight:bold;">Kent Food Bank</h3>
+            <a href="http://thunderbirds.greenrivertech.net/index.php">
+            <img src="../images/kfb-100x112.png" width="100" height="112" alt="Kent Food Bank Logo"</a>
+        </div>    
+    
     </div>
     
         
 
-<script type="text/javascript">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<!-- jquery library -->
+		<script src="../scripts/jquery-1.11.3.js"></script> 
+		
+		<!-- jQuery UI library -->
+		<script src="../scripts/jquery-ui/jquery-ui.min.js"></script>
+		
+		<script type="text/javascript">
 			//shorthand version - everybody uses this one in real life
 			$(function(){
 				$( "#portfolio" ).accordion({
@@ -34,4 +54,4 @@
 				});
 			});			
 			
-		</script>   
+		</script>        
