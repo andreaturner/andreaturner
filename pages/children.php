3acba54803219345c482d6a9d456dceccf12021b
Paige Israel <paige.israel@yahoo.com>
2016-06-21 13:55:15 -0700

    random files from wordman

---
+++ pages/children.php
@@ -0,0 +1,19 @@
+<?php
+    $page_title = 'Children\'s Books';
+    include ('../shared/header.html');
+?>
+
+    <div class="books-page">
+        <div class="keep-it-together">
+            
+                <div class="header-novel">
+                    <a href="under-construction.php"><span>C</span>hildren's <span>B</span>ooks</a>
+                </div>
+                <div class="series-pic-books-novels">
+                    <a href="under-construction.php"><img src="../images/children-900x711.jpg" width="900" height="711" alt="Children's Boooks Image" /></a>
+                </div>
+             <!-- end text-left-books div -->
+            
+<?php
+    include ('../shared/footer.html');            
+?>
