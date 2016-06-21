2d4e2b57e3ab8126d0b028aa7cdcb6313cb12b59
Paige Israel <paige.israel@yahoo.com>
2016-06-21 13:54:57 -0700

    Started updating PaigeIsrael.com
    Getting my own site up and running!  Yahoo!

---
+++ andreaturner-master/andreaturner-master/index.php
@@ -0,0 +1,45 @@
+<?php
+    $page_title = 'Paige Israel';
+    include ('/shared/header.html');
+?>
+        
+            <div class="content">
+		
+		<span class="span">What can I do for you?</span>
+		
+		    <ul>
+			<li>Have you outgrown your Website?</li>
+			<li>Do you need a new look?</li>
+			<li>Does your content need updating?</li>
+			<li>Is your Site outdated?</li>
+		    </ul>
+		    
+		    <p style="color: #3A01DF; font-size: 1.5em; text-align: center;">
+		     I can help you with all of these things!  
+		    </p>
+		    
+		    <p>
+		     My goal is to work with you to ensure that your Web presence represents your best self;<br> whether you're an artist or business
+		     person, or an artist with a business. We all have a vision of how we would like to be portrayed and I help you identify this
+		     vision and express<br> it using cutting edge technology.
+		    </p>
+		    
+		    <p>
+		    I am a kind, quirky, hard-working perfectionist who is passionate about giving people<br> the opportunity
+		    to express themselves and showcase thier work or business via the<br> fabulous medium of the World Wide Web using my talent 
+		    and skill as a Web Developer.
+		    </p>
+		    
+		    <p>
+		    Whether you need a new Web Site developed, or have a current ite that needs a remodel<br><span style="color: #088A08; font-size: 1.5em; text-align: left;
+		    padding-left: 120px;">I am the Developer for YOU!</span>
+		    </p>
+		    
+		    <p>
+		    <a href="contact.html" alt="Contact link">Contact me </a>and we can talk to guarantee that your Web Site is all you desire!
+		    </p> 
+            </div>
+        
+    </body>
+        
+</html>
